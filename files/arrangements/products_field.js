4cee44ec66eec24bcdede3005179119c044b191e
Dmitriy <dmitrijprocevnikov@gmail.com>
2023-12-06 14:19:39 +0300

    заявки

--- files/arrangements/products_field.js
+++
@@ -1,5 +0,0 @@
-const product = document.createElement("div");
-product.setAttribute("class", "product");
-product.innerHTML =
-  '<img class="img" src="./products/Magic_moment.png" alt="Magic_moment" /><audio class="audio" controls><source src="./products/Magic_moment.mp3" type="audio/mpeg" /></audio><button class="top_button">В корзину</button>';
-products_field.append(product);
