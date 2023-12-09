cf62265b2b596659859bd935ecd64815d7c24bfc
Dmitriy <dmitrijprocevnikov@gmail.com>
2023-12-09 22:01:56 +0300

    форма заявки

---
+++ files/enroll/fetch.js
@@ -0,0 +1,32 @@
+token = "6815622159:AAEi0Xp1753lx2Iz--Kp-7GjJlYWevjodho";
+chat_id = "-1001898298001";
+
+let text = "";
+
+function serializeForm(formNode) {
+  const { elements } = formNode;
+
+  const data = new FormData();
+
+  Array.from(elements)
+    .filter((item) => !!item.name)
+    .forEach((element) => {
+      const { name, type } = element;
+      const value = type === "checkbox" ? element.checked : element.value;
+
+      data.append(name, value);
+      text += `[${name}: -- ${value}] \n`;
+    });
+  console.log(text);
+  fetch(
+    `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&parse_mode=html&text=${text}`
+  );
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  serializeForm(applicantForm);
+}
+
+const applicantForm = document.getElementById("mars-once");
+applicantForm.addEventListener("submit", handleFormSubmit);
