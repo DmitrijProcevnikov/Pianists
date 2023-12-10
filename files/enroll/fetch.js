c248e149b97776375d13f416b9f908a4bf7c7f80
Dmitriy <dmitrijprocevnikov@gmail.com>
2023-12-10 21:59:03 +0300

    аранжировки

--- files/enroll/fetch.js
+++ files/enroll/fetch.js
@@ -3,6 +3,22 @@ chat_id = ${{ secrets.CHAT_ID }};
 
 let text = "";
 
+const okMessage = document.querySelector(".ok-message");
+const errMessage = document.querySelector(".err-message");
+const logForm = document.querySelector(".log-form");
+const applicantForm = document.getElementById("mars-once");
+applicantForm.addEventListener("submit", handleFormSubmit);
+
+function onSuccess() {
+  logForm.style.display = "none";
+  okMessage.style.display = "block";
+}
+
+function errSuccess() {
+  logForm.style.display = "none";
+  errMessage.style.display = "block";
+}
+
 function serializeForm(formNode) {
   const { elements } = formNode;
 
@@ -18,15 +34,25 @@ function serializeForm(formNode) {
       text += `${name}: -- ${value} %0A`;
     });
   console.log(text);
-  fetch(
-    `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&parse_mode=html&text=${text}`
+}
+async function sendData(data) {
+  return await fetch(
+    `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&parse_mode=html&text=${text}`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "multipart/form-data" },
+      body: data,
+    }
   );
 }
 
-function handleFormSubmit(event) {
+async function handleFormSubmit(event) {
   event.preventDefault();
-  serializeForm(applicantForm);
+  const data = serializeForm(event.target);
+  const { status, error } = await sendData(data);
+  if (status === 200) {
+    onSuccess();
+  } else {
+    errSuccess();
+  }
 }
-
-const applicantForm = document.getElementById("mars-once");
-applicantForm.addEventListener("submit", handleFormSubmit);
