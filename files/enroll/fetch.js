3ec708bf0d3e5bfef78ca3a83f3601235f245228
Дмитрий Процевников <92599527+DmitrijProcevnikov@users.noreply.github.com>
2023-12-11 15:13:28 +0300

    Update fetch.js  тест2

--- files/enroll/fetch.js
+++ files/enroll/fetch.js
@@ -1,5 +1,5 @@
-token = `${{ secrets.TOKEN_TELEGRAM }}`;
-chat_id = `${{ secrets.CHAT_ID }}`;
+token = ${{ secrets.TOKEN_TELEGRAM }};
+chat_id = ${{ secrets.CHAT_ID }};
 
 let text = "Привет!";
 
@@ -39,12 +39,12 @@ let text = "Привет!";
 //   return await 
 // }
 fetch(
-    `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&parse_mode=html&text=${text}`,
-    {
-      method: "POST",
-      headers: { "Content-Type": "multipart/form-data" },
-      body: data,
-    }
+    `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&parse_mode=html&text=${text}`
+    // {
+    //   method: "POST",
+    //   headers: { "Content-Type": "multipart/form-data" },
+    //   body: data,
+    // }
   );
 // async function handleFormSubmit(event) {
 //   event.preventDefault();
